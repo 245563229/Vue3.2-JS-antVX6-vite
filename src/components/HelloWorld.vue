2e01d223e0e1e4b0c90491c53e43f680802b3f39
“gefanghe” <aa208208@qq.com>
2022-09-23 16:31:00 +0800

    定一个图形及连线

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,12 @@
 <script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
+import { ref } from "vue";
+const count = ref(0);
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
+  <div class="card"></div>
 
   <p>
     Check out
